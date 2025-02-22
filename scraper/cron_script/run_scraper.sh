db5d9ef9787548f7fff8ec1ee3b4cfd7b0c4cb16
Alex Kaiser <alexkaiser@me.com>
2025-02-22 17:20:38 +0000

    Adds more verbose log messages

--- scraper/cron_script/run_scraper.sh
+++ scraper/cron_script/run_scraper.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Add timestamp to log
+echo "====================================="
+date
+echo "Starting scraper..."
+
 # Navigate to the project directory
 cd /home/ubuntu/LinkedIn-Job-Scraper
 
 # Run the Python script
 python3 main.py
+
+# Log completion
+echo "Scraper finished at: "
+date
+echo "====================================="
